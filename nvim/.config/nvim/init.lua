75067b698801fa8bbf64e82205335ee81b9003c5
Hayes Barber <barberhayes@gmail.com>
2025-10-05 18:52:14 -0400

    auto commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,3 +5,12 @@ vim.o.relativenumber = true
 
 vim.cmd('nmap <leader>w :w<cr>')
 vim.cmd('nmap <leader>q :q<cr>')
+
+if vim.g.vscode then
+  local vscode = require('vscode')
+
+  vim.keymap.set('n', '<leader>e', function()
+    vscode.action('workbench.view.explorer')
+  end, { desc = 'Focus VSCode Explorer' })
+
+end
