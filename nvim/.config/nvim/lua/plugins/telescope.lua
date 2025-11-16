0a2216d31ca6df9c151bd0af0501a022d805914d
Hayes Barber <barberhayes@gmail.com>
2025-11-16 13:31:08 -0500

    auto commit

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,11 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = 'v0.1.9',
+        cond = function()
+            return not vim.g.vscode
+        end,
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
-            if vim.g.vscode then
-                return
-            end
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>f', function()
                 builtin.find_files({ hidden = true })
