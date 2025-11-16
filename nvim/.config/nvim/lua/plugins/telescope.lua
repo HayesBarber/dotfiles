070cde984ea3aed68b143b5311836b8d88f175c8
Hayes Barber <barberhayes@gmail.com>
2025-11-16 14:12:29 -0500

    fix formatting

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,19 +1,17 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = 'v0.1.9',
-        cond = function()
-            return not vim.g.vscode
-        end,
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>f', function()
-                builtin.find_files({
-                    hidden = true,
-                    file_ignore_patterns = { ".git/" },
-                })
-            end)
-        end,
-    },
+    'nvim-telescope/telescope.nvim',
+    tag = 'v0.1.9',
+    cond = function()
+        return not vim.g.vscode
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>f', function()
+            builtin.find_files({
+                hidden = true,
+                file_ignore_patterns = { ".git/" },
+            })
+        end)
+    end,
 }
