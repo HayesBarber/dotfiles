1ee540a28aab35dc2a0282ab839c11438480a555
HayesBarber <barberhayes@gmail.com>
2025-11-16 21:02:59 -0500

    update terminal mapping

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -7,15 +7,7 @@ return {
     config = function()
         require("toggleterm").setup({
             direction = "float",
-            open_mapping = [[<leader>t]],
+            open_mapping = [[<C-j>]],
         })
-        vim.keymap.set('t', '<Esc>', function()
-            vim.api.nvim_feedkeys(
-                vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true),
-                "n",
-                false
-            )
-            vim.cmd("ToggleTerm")
-        end, { noremap = true, silent = true })
     end,
 }
