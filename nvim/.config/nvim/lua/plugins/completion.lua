692425cf330cad59413c714d3d79c32a384f1ae0
Hayes Barber <barberhayes@gmail.com>
2025-11-16 19:21:19 -0500

    add completion

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,20 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    cond = function()
+        return not vim.g.vscode
+    end,
+    version = '1.*',
+    opts = {
+        keymap = { preset = 'super-tab' },
+        appearance = {
+            nerd_font_variant = 'mono'
+        },
+        completion = { documentation = { auto_show = true } },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+}
