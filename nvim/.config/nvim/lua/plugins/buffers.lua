9e62257fb8f33d48a178d2e9624797aac2171e4d
Hayes Barber <barberhayes@gmail.com>
2025-11-17 20:29:07 -0500

    load buffer condition

--- nvim/.config/nvim/lua/plugins/buffers.lua
+++ nvim/.config/nvim/lua/plugins/buffers.lua
@@ -1,6 +1,9 @@
 return {
     'akinsho/bufferline.nvim',
     version = "*",
+    cond = function()
+        return not vim.g.vscode
+    end,
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {},
 }
