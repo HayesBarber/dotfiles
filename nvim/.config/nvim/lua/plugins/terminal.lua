58bf672bdb5dba36ade81aa9cd5b26fd49d520fa
Hayes Barber <barberhayes@gmail.com>
2025-11-18 20:22:48 -0500

    use opts in terminal.lua

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -4,10 +4,8 @@ return {
     cond = function()
         return not vim.g.vscode
     end,
-    config = function()
-        require("toggleterm").setup({
-            direction = "float",
-            open_mapping = [[<C-j>]],
-        })
-    end,
+    opts = {
+        direction = "float",
+        open_mapping = [[<C-j>]],
+    },
 }
