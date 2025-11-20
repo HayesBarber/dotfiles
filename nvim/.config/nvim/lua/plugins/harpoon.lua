6c07baccdbe858a80bcac19a43eee7b51fcebf19
Hayes Barber <barberhayes@gmail.com>
2025-11-19 20:10:23 -0500

    format all files

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,46 +1,49 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    cond = function()
-        return not vim.g.vscode
-    end,
-    config = function()
-        local harpoon = require("harpoon")
-        harpoon:setup({})
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	cond = function()
+		return not vim.g.vscode
+	end,
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup({})
 
-        vim.keymap.set("n", "<leader>H", function()
-            harpoon:list():add()
-        end)
-        vim.keymap.set("n", "<C-h>", function()
-            harpoon:list():clear()
-        end)
+		vim.keymap.set("n", "<leader>H", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():clear()
+		end)
 
-        for i = 1, 5 do
-            vim.keymap.set("n", "<leader>" .. i, function()
-                harpoon:list():select(i)
-            end)
-        end
+		for i = 1, 5 do
+			vim.keymap.set("n", "<leader>" .. i, function()
+				harpoon:list():select(i)
+			end)
+		end
 
-        -- basic telescope configuration
-        local conf = require("telescope.config").values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
 
-            require("telescope.pickers").new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
-                previewer = conf.file_previewer({}),
-                sorter = conf.generic_sorter({}),
-            }):find()
-        end
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
 
-        vim.keymap.set("n", "<C-j>", function() toggle_telescope(harpoon:list()) end)
-    end,
+		vim.keymap.set("n", "<C-j>", function()
+			toggle_telescope(harpoon:list())
+		end)
+	end,
 }
-
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,34 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = 'v0.1.9',
-    cond = function()
-        return not vim.g.vscode
-    end,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.1.9",
+	cond = function()
+		return not vim.g.vscode
+	end,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
 
-        vim.keymap.set('n', '<leader>f', function()
-            builtin.find_files({
-                hidden = true,
-                file_ignore_patterns = { ".git/" },
-            })
-        end)
+		vim.keymap.set("n", "<leader>f", function()
+			builtin.find_files({
+				hidden = true,
+				file_ignore_patterns = { ".git/" },
+			})
+		end)
 
-        vim.keymap.set('n', '<leader>rg', function()
-            builtin.live_grep({
-                file_ignore_patterns = { ".git/" },
-                additional_args = function()
-                    return { "--hidden" }
-                end,
-            })
-        end)
+		vim.keymap.set("n", "<leader>rg", function()
+			builtin.live_grep({
+				file_ignore_patterns = { ".git/" },
+				additional_args = function()
+					return { "--hidden" }
+				end,
+			})
+		end)
 
-        vim.keymap.set('n', '<leader>b', function()
-            builtin.buffers({
-                sort_lastused = true,
-                ignore_current_buffer = true,
-            })
-        end)
-    end,
+		vim.keymap.set("n", "<leader>b", function()
+			builtin.buffers({
+				sort_lastused = true,
+				ignore_current_buffer = true,
+			})
+		end)
+	end,
 }
-
