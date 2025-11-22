82916aa511e96eb7d08df54b59de148c43669891
Hayes Barber <barberhayes@gmail.com>
2025-11-22 15:16:50 -0500

    fix typo

--- nvim/.config/nvim/lua/custom/commands/git-branches.lua
+++ nvim/.config/nvim/lua/custom/commands/git-branches.lua
@@ -35,7 +35,7 @@ end
 
 function M.setup()
 	vim.api.nvim_create_user_command("GitBranch", function()
-		require("custom.commands.git_braches").pick_branch()
+		require("custom.commands.git-branches").pick_branch()
 	end, {})
 end
 
