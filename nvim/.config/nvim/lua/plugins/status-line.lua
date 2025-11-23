d25e7c3d9acdc2f6259bc3dadb90895e94567bf9
HayesBarber <barberhayes@gmail.com>
2025-11-22 23:14:21 -0500

    adjust status line

--- nvim/.config/nvim/lua/plugins/status-line.lua
+++ nvim/.config/nvim/lua/plugins/status-line.lua
@@ -1,22 +1,36 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	cond = function()
-		return not vim.g.vscode
-	end,
-	opts = {
-		options = { theme = "powerline", disabled_filetypes = { "NvimTree" } },
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff" },
-			lualine_c = {
-				"filename",
-				"filesize",
-				require("gitblame").get_current_blame_text,
-			},
-			lualine_x = { "encoding", "progress", "filetype" },
-			lualine_y = { "location" },
-			lualine_z = { "lsp_status" },
-		},
-	},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cond = function()
+        return not vim.g.vscode
+    end,
+    opts = {
+        options = { theme = "powerline", disabled_filetypes = { "NvimTree" } },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff" },
+            lualine_c = {
+                require("gitblame").get_current_blame_text,
+            },
+            lualine_x = { "progress", "filetype" },
+            lualine_y = { "location" },
+            lualine_z = { "lsp_status" },
+        },
+        tabline = {
+            lualine_a = {},
+            lualine_b = {
+                {
+                    'filename',
+                    file_status = true,
+                    path = 1,
+                }
+            },
+            lualine_c = {},
+            lualine_x = {
+                "filesize",
+            },
+            lualine_y = {},
+            lualine_z = {},
+        }
+    },
 }
