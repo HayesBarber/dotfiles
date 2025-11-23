ce2e119c85c1be88f198fd59468b25d846ddd85e
HayesBarber <barberhayes@gmail.com>
2025-11-22 20:52:12 -0500

    add keymaps

--- nvim/.config/nvim/lua/plugins/git-diff.lua
+++ nvim/.config/nvim/lua/plugins/git-diff.lua
@@ -1,1 +1,7 @@
-return { "sindrets/diffview.nvim" }
+return {
+    "sindrets/diffview.nvim",
+    keys = {
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>",  desc = "Diffview Open" },
+        { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+    },
+}
