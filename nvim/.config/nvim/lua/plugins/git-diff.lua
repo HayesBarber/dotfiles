a2a30507870f6f1c5c675e4f852699c3017fa3a0
HayesBarber <barberhayes@gmail.com>
2025-11-23 22:47:57 -0500

    auto commit

--- nvim/.config/nvim/lua/plugins/git-diff.lua
+++ nvim/.config/nvim/lua/plugins/git-diff.lua
@@ -1,5 +1,8 @@
 return {
     "sindrets/diffview.nvim",
+    cond = function()
+        return not vim.g.vscode
+    end,
     keys = {
         { "<leader>gd", "<cmd>DiffviewOpen<cr>",  desc = "Diffview Open" },
         { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
