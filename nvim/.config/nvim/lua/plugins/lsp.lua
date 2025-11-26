21bfa9976338f1657469df8270cd473eb7a85f74
HayesBarber <barberhayes@gmail.com>
2025-11-25 21:29:14 -0500

    go to definition

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	cond = function()
-		return not vim.g.vscode
-	end,
-	dependencies = {
-		{ "mason-org/mason.nvim", opts = {} },
-		"neovim/nvim-lspconfig",
-	},
-	opts = {},
+    "mason-org/mason-lspconfig.nvim",
+    cond = function()
+        return not vim.g.vscode
+    end,
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    opts = {},
 }
