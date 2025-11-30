34cb91dd176b9b952135f37ae2c4871bde332c90
Hayes Barber <barberhayes@gmail.com>
2025-11-30 16:31:08 -0500

    update theme

--- nvim/.config/nvim/lua/plugins/status-line.lua
+++ nvim/.config/nvim/lua/plugins/status-line.lua
@@ -1,38 +1,38 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cond = function()
-        return not vim.g.vscode
-    end,
-    opts = {
-        options = { theme = "powerline", disabled_filetypes = { "NvimTree", "toggleterm" } },
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = {
-                require("gitblame").get_current_blame_text,
-            },
-            lualine_x = { "progress", "filetype" },
-            lualine_y = { "location" },
-            lualine_z = { "lsp_status" },
-        },
-        tabline = {
-            lualine_a = {
-                "buffers"
-            },
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {
-                "filesize",
-            },
-            lualine_y = {
-                {
-                    "filename",
-                    file_status = true,
-                    path = 1,
-                }
-            },
-            lualine_z = {},
-        }
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cond = function()
+		return not vim.g.vscode
+	end,
+	opts = {
+		options = { theme = "powerline", disabled_filetypes = { "NvimTree", "toggleterm" } },
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = {
+				require("gitblame").get_current_blame_text,
+			},
+			lualine_x = { "progress", "filetype" },
+			lualine_y = { "location" },
+			lualine_z = { "lsp_status" },
+		},
+		tabline = {
+			lualine_a = {
+				"buffers",
+			},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {
+				"filesize",
+			},
+			lualine_y = {
+				{
+					"filename",
+					file_status = true,
+					path = 1,
+				},
+			},
+			lualine_z = {},
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,11 @@
 return {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    cond = function()
-        return not vim.g.vscode
-    end,
-    config = function()
-        require('onedark').setup {
-            style = 'warmer'
-        }
-        require('onedark').load()
-    end
+	"Mofiqul/vscode.nvim",
+	priority = 1000,
+	cond = function()
+		return not vim.g.vscode
+	end,
+	config = function()
+		require("vscode").setup({})
+		vim.cmd.colorscheme("vscode")
+	end,
 }
