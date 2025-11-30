a7d5d294578870fc20584874bbadb71585ea2aae
Hayes Barber <barberhayes@gmail.com>
2025-11-30 12:18:15 -0500

    change quit_on_open to false

--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -1,33 +1,33 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    cond = function()
-        return not vim.g.vscode
-    end,
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-        actions = {
-            open_file = {
-                quit_on_open = true,
-            },
-        },
-        update_focused_file = {
-            enable = true,
-        },
-        view = {
-            adaptive_size = true,
-        },
-    },
-    keys = {
-        {
-            "<leader>e",
-            function()
-                require("nvim-tree.api").tree.toggle({ focus = true })
-            end,
-            desc = "Toggle NvimTree",
-        },
-    },
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	cond = function()
+		return not vim.g.vscode
+	end,
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		actions = {
+			open_file = {
+				quit_on_open = false,
+			},
+		},
+		update_focused_file = {
+			enable = true,
+		},
+		view = {
+			adaptive_size = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("nvim-tree.api").tree.toggle({ focus = true })
+			end,
+			desc = "Toggle NvimTree",
+		},
+	},
 }
