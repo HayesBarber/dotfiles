96f7bcc25c158ac6677ac2b795a3ae8747333190
Hayes Barber <barberhayes@gmail.com>
2025-12-15 18:57:19 -0500

    config function

--- nvim/.config/nvim/lua/plugins/multi-cursor.lua
+++ nvim/.config/nvim/lua/plugins/multi-cursor.lua
@@ -4,4 +4,21 @@ return {
 	cond = function()
 		return not vim.g.vscode
 	end,
+	config = function()
+		local mc = require("multicursor-nvim")
+		mc.setup()
+		local set = vim.keymap.set
+		set({ "n", "x" }, "<up>", function()
+			mc.lineAddCursor(-1)
+		end)
+		set({ "n", "x" }, "<down>", function()
+			mc.lineAddCursor(1)
+		end)
+		set({ "n", "x" }, "<leader>A", mc.matchAllAddCursors)
+		mc.addKeymapLayer(function(layerSet)
+			layerSet("n", "<esc>", function()
+				mc.clearCursors()
+			end)
+		end)
+	end,
 }
