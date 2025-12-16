a65acab0e53fd3949d09fb50f92f08305e596daf
William Hayes Barber <barberhayes@gmail.com>
2025-12-16 17:13:04 -0500

    git-delta-performance-improvement (#25)

---
+++ nvim/.config/nvim/lua/custom/commands/git-delta.lua
@@ -0,0 +1,27 @@
+local cache = {
+	value = "",
+	last = 0,
+}
+
+local TTL = 10
+
+local function git_delta()
+	local now = vim.uv.now() / 1000
+
+	if now - cache.last < TTL then
+		return cache.value
+	end
+
+	cache.last = now
+	if vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null"):match("true") == nil then
+		cache.value = ""
+	else
+		local output = vim.fn.systemlist("git status --porcelain")
+		local count = #output
+		cache.value = ("Δ " .. count)
+	end
+
+	return cache.value
+end
+
+return git_delta
