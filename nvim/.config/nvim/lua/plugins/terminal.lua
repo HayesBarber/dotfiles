16f8a60aa39678bc7bd43a5fb686b2e849dce885
HayesBarber <barberhayes@gmail.com>
2025-12-25 14:09:07 -0500

    edit terminal config

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -1,12 +1,23 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	cond = function()
-		return not vim.g.vscode
-	end,
-	opts = {
-		direction = "vertical",
-		size = vim.o.columns * 0.5,
-		open_mapping = [[<C-j>]],
-	},
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    cond = function()
+        return not vim.g.vscode
+    end,
+    config = function()
+        require("toggleterm").setup({
+            direction = "vertical",
+            size = function()
+                return math.floor(vim.o.columns * 0.5)
+            end,
+            open_mapping = [[<C-j>]],
+        })
+
+        vim.api.nvim_create_autocmd("TermOpen", {
+            pattern = "term://*toggleterm#*",
+            callback = function()
+                vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = true })
+            end,
+        })
+    end,
 }
