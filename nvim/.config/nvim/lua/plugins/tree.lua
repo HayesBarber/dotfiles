29d0fe00efbc2eddfb14959d2d6e7fa60e8dfac4
HayesBarber <barberhayes@gmail.com>
2025-12-27 14:16:16 -0500

    auto commit

--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -1,29 +1,33 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	cond = function()
-		return not vim.g.vscode
-	end,
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			actions = {
-				open_file = {
-					quit_on_open = false,
-				},
-			},
-			update_focused_file = {
-				enable = true,
-			},
-			view = {
-				adaptive_size = true,
-			},
-		})
-		vim.keymap.set("n", "<leader>e", function()
-			require("nvim-tree.api").tree.toggle({ focus = true })
-		end)
-	end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    cond = function()
+        return not vim.g.vscode
+    end,
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup({
+            actions = {
+                open_file = {
+                    quit_on_open = false,
+                },
+            },
+            update_focused_file = {
+                enable = true,
+            },
+            view = {
+                adaptive_size = true,
+            },
+            git = {
+                enable = true,
+                ignore = false,
+            },
+        })
+        vim.keymap.set("n", "<leader>e", function()
+            require("nvim-tree.api").tree.toggle({ focus = true })
+        end)
+    end,
 }
