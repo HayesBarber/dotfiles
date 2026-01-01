ca7d2aa07a054316db221d2346c6504efd275228
HayesBarber <barberhayes@gmail.com>
2026-01-01 15:33:19 -0500

    check if buffer is a terminal

--- nvim/.config/nvim/lua/custom/commands/non-tree-buffers.lua
+++ nvim/.config/nvim/lua/custom/commands/non-tree-buffers.lua
@@ -1,44 +1,50 @@
 local M = {}
 
-function M.get_non_tree_buffers()
-	local bufs = {}
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype ~= "nofile" then
-			local name = vim.api.nvim_buf_get_name(buf)
-			if not name:match("NvimTree_") then
-				table.insert(bufs, buf)
-			end
-		end
-	end
-	return bufs
+function M.get_normal_buffers()
+    local bufs = {}
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
+            local bt = vim.bo[buf].buftype
+            local name = vim.api.nvim_buf_get_name(buf)
+
+            if bt ~= "nofile"
+                and bt ~= "terminal"
+                and not name:match("NvimTree_")
+                and not name:match("^term://")
+            then
+                table.insert(bufs, buf)
+            end
+        end
+    end
+    return bufs
 end
 
 function M.close_other_buffers()
-	local current = vim.api.nvim_get_current_buf()
+    local current = vim.api.nvim_get_current_buf()
 
-	for _, buf in ipairs(M.get_non_tree_buffers()) do
-		if buf ~= current then
-			vim.api.nvim_buf_delete(buf, { force = false })
-		end
-	end
+    for _, buf in ipairs(M.get_normal_buffers()) do
+        if buf ~= current then
+            vim.api.nvim_buf_delete(buf, { force = false })
+        end
+    end
 end
 
 function M.setup()
-	if vim.g.vscode then
-		return
-	end
-	vim.api.nvim_create_user_command("BufClose", function()
-		local non_tree_bufs = M.get_non_tree_buffers()
+    if vim.g.vscode then
+        return
+    end
+    vim.api.nvim_create_user_command("BufClose", function()
+        local non_tree_bufs = M.get_normal_buffers()
 
-		if #non_tree_bufs <= 1 then
-			vim.cmd("bd")
-		else
-			vim.cmd("bprevious")
-			vim.cmd("bd #")
-		end
-	end, {})
-	vim.keymap.set("n", "<leader>q", ":BufClose<CR>", { desc = "Close buffer" })
-	vim.keymap.set("n", "<leader>a", M.close_other_buffers, { desc = "Close all buffers except current" })
+        if #non_tree_bufs <= 1 then
+            vim.cmd("bd")
+        else
+            vim.cmd("bprevious")
+            vim.cmd("bd #")
+        end
+    end, {})
+    vim.keymap.set("n", "<leader>q", ":BufClose<CR>", { desc = "Close buffer" })
+    vim.keymap.set("n", "<leader>a", M.close_other_buffers, { desc = "Close all buffers except current" })
 end
 
 return M
