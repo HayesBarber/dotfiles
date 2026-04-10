9ae0f3e2ffe5b488410944a767b2fe29394aadb7
Hayes Barber <barberhayes@gmail.com>
2026-04-09 20:40:22 -0400

    update ghostty ssh config

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,11 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	priority = 1000,
 	cond = function()
 		return not vim.g.vscode
 	end,
 	config = function()
-		vim.cmd.colorscheme("kanagawa-dragon")
+		vim.cmd.colorscheme("rose-pine")
 	end,
 }
