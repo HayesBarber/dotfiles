203f4a9e9822dc5ad2c4afd7d560f8a5ebd02fd4
Hayes Barber <barberhayes@gmail.com>
2026-04-23 21:24:30 -0400

    cleanup

--- nvim/.config/nvim/lua/plugins/git-diff.lua
+++ nvim/.config/nvim/lua/plugins/git-diff.lua
@@ -1,10 +1,10 @@
 return {
-    "sindrets/diffview.nvim",
-    cond = function()
-        return not vim.g.vscode
-    end,
-    keys = {
-        { "<leader>gd", "<cmd>DiffviewOpen<cr>",  desc = "Diffview Open" },
-        { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
-    },
+	"sindrets/diffview.nvim",
+	cond = function()
+		return not vim.g.vscode
+	end,
+	keys = {
+		{ "gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+		{ "gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+	},
 }
