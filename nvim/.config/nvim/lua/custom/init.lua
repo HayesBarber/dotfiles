fd92f3c0b244acac3e5d1e16cc8ee68cdb9cd6cf
Hayes Barber <barberhayes@gmail.com>
2026-04-23 21:36:43 -0400

    cleaning up vscode refs

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,10 +1,8 @@
 local M = {}
 
 function M.setup()
-    if not vim.g.vscode then
-        require("custom.commands.lsp-dart").setup()
-        require("custom.commands.non-tree-buffers").setup()
-    end
+	require("custom.commands.lsp-dart").setup()
+	require("custom.commands.non-tree-buffers").setup()
 end
 
 return M
