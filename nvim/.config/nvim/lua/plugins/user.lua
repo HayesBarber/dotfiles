f53d4f95a63f9949219b8f9cdea0b7533a1ba3b1
Hayes Barber <barberhayes@gmail.com>
2026-04-25 20:07:20 -0400

    update plugins

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -7,4 +7,29 @@ return {
     config = function() require("lsp_signature").setup() end,
   },
 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    opts = function(_, opts)
+      vim.api.nvim_create_autocmd("TermOpen", {
+        pattern = "term://*toggleterm#*",
+        callback = function()
+				  vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = true })
+			  end,
+      })
+
+      return opts
+    end,
+  },
+
 }
