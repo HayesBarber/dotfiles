a8e89ae58931e1062992cc8037761bd75e729305
Hayes Barber <barberhayes@gmail.com>
2026-04-25 20:15:59 -0400

    add flash

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -32,4 +32,19 @@ return {
     end,
   },
 
+	 {
+	  "folke/flash.nvim",
+	  event = "VeryLazy",
+	  opts = {},
+	  keys = {
+		  {
+			  "s",
+			  mode = { "n", "x", "o" },
+			  function()
+				  require("flash").jump()
+			  end,
+			  desc = "Flash",
+		  },
+	},},
+
 }
